fd73c519546ad700d3a8160aa43edda01b3c5a56
anderson <anderson.valencia@udea.edu.co>
2022-08-19 22:26:43 -0500

    add read_expression function

--- derive.py
+++ derive.py
@@ -18,6 +18,12 @@ def simplify(regex):
     temp_2 = [i for i in temp if '∅' not in i]
     return '∅' if len(temp_2) == 0 else '+'.join([remove_epsilon(i) for i in temp_2])
 
+def remove_parenthesis(regex):
+    while regex.startswith('(') and regex.endswith(')'):
+        regex = regex[1:]
+        regex = regex[:-1]
+    return regex
+
 def derive(regex, character):
 
     regex = regex.replace(' ', '')
@@ -31,13 +37,47 @@ def derive(regex, character):
     elif len(regex) == 1:
         return '∅'
 
-    elif '+' in regex:
-        temp = regex.split('+')
-        temp_2 = list(map(lambda x: derive(x, character), temp))
-        return simplify('+'.join(temp_2))
+    #elif '+' in regex:
+    #    temp = regex.split('+')
+    #    temp_2 = list(map(lambda x: derive(x, character), temp))
+    #    return simplify('+'.join(temp_2))
     
     elif len(regex) > 1 and not (len(regex) == 2 and regex.endswith('*')):
         return regex[1:] if regex.startswith(character) else '∅'
 
     elif '*' in regex:
         return simplify(derive(regex[:-1], character) + '('  + regex + ')')
+
+
+def read_expresion(regex, character):
+    regex = remove_parenthesis(regex)
+    temp = regex.split('+')
+    temp_2 = []
+    expression = ''
+    parenthesis = False
+    for i in range(len(temp)):
+        if '(' in temp[i] and not ')' in temp[i] or parenthesis:
+            if ')' in temp[i]:
+                if len(expression) == 0:
+                    expression = expression + temp[i]
+                else:
+                    expression = expression + '+' + temp[i]
+                parenthesis = False
+                temp_2.append(expression)
+            else:
+                if len(expression) == 0:
+                    expression = expression + temp[i]
+                else:
+                    expression = expression + '+' + temp[i]
+                parenthesis = True
+        else:
+            temp_2.append(temp[i])
+            parenthesis = False
+            expression = ''
+        
+    temp_3 = list(map(lambda x: derive(x, character), temp_2))
+    return simplify('+'.join(temp_3))
+
+
+print(read_expresion('(ab)b', 'a'))
+print(read_expresion('(ab)(ad)', 'a'))
